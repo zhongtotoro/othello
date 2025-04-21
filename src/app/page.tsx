77fa90452ee3b436e8fa2ea28ad79433445f7e79
zhongtotoro <s1F102400055@iniad.org>
2025-04-21 19:20:30 +0900

    'オセロの色が変わるようになった’

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,47 @@
 'use client';
 
+import { useState } from 'react';
 import styles from './page.module.css';
 
 export default function Home() {
+  const [board, setBoard] = useState([
+    [1, 2, 1, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+  const [turnColor, setTurnColor] = useState(1);
+
+  const clickhundler = (x: number, y: number) => {
+    const newBoard = structuredClone(board);
+    newBoard[y][x] = turnColor;
+    setBoard(newBoard);
+    if (turnColor === 1) {
+      setTurnColor(2);
+    } else {
+      setTurnColor(1);
+    }
+  };
   return (
     <div className={styles.container}>
-      <div className={styles.board}>a</div>;
+      <div className={styles.board}>
+        {board.map((row, y) =>
+          row.map((color, x) => (
+            <div className={styles.cell} key={`${x}-${y}`} onClick={() => clickhundler(x, y)}>
+              {board[y][x] !== 0 && (
+                <div
+                  className={styles.stone}
+                  style={{ background: color === 1 ? '#000' : '#FFF' }}
+                />
+              )}
+            </div>
+          )),
+        )}
+      </div>
     </div>
   );
 }
